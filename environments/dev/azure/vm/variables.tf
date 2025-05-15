76aac32cbdebc39212f3dd5ba7267c2a6ba87ca8
Luiz Henrique Arantes <79605326+lharantes@users.noreply.github.com>
2025-05-15 11:32:35 +0100

    Update variables.tf

--- environments/dev/azure/vm/variables.tf
+++ environments/dev/azure/vm/variables.tf
@@ -1,7 +1,4 @@
-variable "subscription_id" {
-  description = "The name of the resource group"
-  type        = string
-}
+
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
